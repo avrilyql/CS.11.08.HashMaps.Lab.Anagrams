e66e4beed63e04526a11e2e9b07f2ee8599d1f7a
avrilyql <avrilyql2027@student.cis.edu.hk>
2025-03-12 12:04:15 +0800

    donedonedoned

--- src/main/java/AnagramSolver.java
+++ src/main/java/AnagramSolver.java
@@ -1,5 +1,9 @@
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.Scanner;
 
 public class AnagramSolver {
 
@@ -11,8 +15,23 @@ public class AnagramSolver {
      * @param filename
      * @return
      */
-    public static HashMap<String, ArrayList<String>> anagrams(String filename) {
-        return null;
+    public static HashMap<String, ArrayList<String>> anagrams(String filename) throws FileNotFoundException {
+        HashMap<String,ArrayList<String>> hm = new HashMap<>();
+        Scanner scanner = new Scanner(new File(filename));
+
+        while (scanner.hasNextLine()){
+            String word = scanner.nextLine();
+            //seperate word into char array for sorting
+            char[] chars = word.toCharArray();
+            Arrays.sort(chars);
+            String sorted = new String(chars);
+            //if unfamiliar then add to hm
+            if (!hm.containsKey(sorted)) {
+                hm.put(sorted, new ArrayList<>());
+            }
+            hm.get(sorted).add(word);
+        }
+        return hm;
     }
 
     /**
@@ -22,7 +41,16 @@ public class AnagramSolver {
      * @return
      */
     public static ArrayList<String> mostFrequentAnagram(HashMap<String, ArrayList<String>> anagrams) {
-        return null;
+        ArrayList<String> biggest = new ArrayList<>(); //arraylist for biggest anagram
+        ArrayList<ArrayList<String>> values =  new ArrayList<ArrayList<String>>(anagrams.values()); //arraylist of values
+        //iterate through values
+        for (int i = 0; i < values.size(); i++) {
+            ArrayList<String> value = values.get(i);
+            if (value.size() > biggest.size()) {
+                biggest = value; //change biggest value
+            }
+        }
+        return biggest;
     }
 
     /**
@@ -31,7 +59,9 @@ public class AnagramSolver {
      * @param anagrams
      */
     public static void printKeyValuePairs(HashMap<String, ArrayList<String>> anagrams) {
-
+        for (String key: anagrams.keySet()) { //iterate thru keys
+            System.out.println(key + ":" + anagrams.get(key));
+        }
     }
 
 }
